a700f6fabfa2eba50f52f06f0fac0ea86a2e80a3
isham ansari <59247202+ishamansari@users.noreply.github.com>
2023-06-25 18:46:03 +0530

    Update Calculator.js

--- src/Pages/Calculator.js
+++ src/Pages/Calculator.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 
 export const Calculator = () =>{
-
+    const [searchParams, setSearchParams] = useSearchParams();
     return(
-        
+        <div>Your calculation is <span id="calc-result">{eval(`${searchParams.get("num1")} ${searchParams.get("op")? searchParams.get("op") : "+"} ${searchParams.get("num2")}`)}</span>
+        </div>
     )
 }
